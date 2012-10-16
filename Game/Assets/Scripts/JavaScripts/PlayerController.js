6ba494fda6a68306f03025148b28c01ccf0a6858
Max Goltzsche <max.goltzsche@gmail.com>
2012-10-17 01:46:26 +0200

    flight script

--- Game/Assets/Scripts/JavaScripts/PlayerController.js
+++ Game/Assets/Scripts/JavaScripts/PlayerController.js
@@ -1,44 +1,15 @@
-function Start() {
-}
+var maxTorque : float = 1.0f;
+var forwardForce : float = 10.0f;
+
+var currentTorqueHor : float = 0.0f;
+var currentTorqueVer : float = 0.0f;
 
 function Update() {
-	//transform.Translate(0, Input.GetAxis("Horizontal"), 0);
-	//transform.Rotate(Input.GetAxis("Vertical"),Input.GetAxis("Horizontal"),0);
-	
-	//transform.Rotate(Input.GetAxis("Vertical"),Input.GetAxis("Horizontal"),0);
-	
-	/*var controller : CharacterController = GetComponent(CharacterController);
-	
-    // Rotate around y - axis
-    //transform.Rotate(0, Input.GetAxis("Horizontal") * 5.0, 0);
-    
-    // Move forward / backward
-    var relativeVelocity: Vector3 = transform.InverseTransformDirection(crigidbody.velocity);
-    var move : Vector3 = Vector3(0, 5.0 * Input.GetAxis("Vertical"), 0);
-    move += Vector3(0, 5.0 * Input.GetAxis("Vertical"), 0);
-    
-    controller.Move(move);*/
-    
-    
+   currentTorqueHor = maxTorque * Input.GetAxis("Horizontal");
+   currentTorqueVer = maxTorque * Input.GetAxis("Vertical");
 }
 
-//@script RequireComponent(CharacterController)
-
 function FixedUpdate() {
-	var acceleration: float = 0.0;
-	var relativePosition: Vector3 = rigidbody.position;
-	var relativeRotation: Quaternion = rigidbody.rotation;
-
-	var relativeVelocity: Vector3 = transform.InverseTransformDirection(rigidbody.velocity);
-	
-	if (Input.GetKey(KeyCode.Q)) {
-		relativeVelocity.z += 3;
-	}
-	
-	if (Input.GetKey(KeyCode.Y)) {
-		relativeVelocity.z -= 3;
-	}
-	
-	relativePosition.z += relativeVelocity.z;
-	rigidbody.MovePosition(relativePosition);
+   rigidbody.AddForce(transform.forward * forwardForce);
+   rigidbody.AddRelativeTorque(currentTorqueVer, 0.0f, -currentTorqueHor);
 }
